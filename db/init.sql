54f6095afeade7a1e90852e08c55f90954e900c0
savianl <lorenzo.savian@corvallis.it>
2021-06-07 17:16:00 +0200

    readme e sql

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,36 @@
-CREATE DATABASE botplatform;
-CREATE DATABASE gateway;
+/*items*/
+create table "TItems"
+(
+	"ItemID" serial not null
+		constraint titems_pk
+			primary key,
+	ASIN varchar,
+	"Titolo" varchar,
+	"Categoria" varchar,
+	"Prezzo" decimal,
+	"Giacenza" int,
+	"Brand" varchar
+);
+
+/*fornitori*/
+create table "TFornitori"
+(
+	"FornitoreID" serial not null
+		constraint tfornitori_pk
+			primary key,
+	"Nome" varchar not null
+);
+
+
+
+/*users*/
+create table "TUsers"
+(
+	"UserID" serial not null
+		constraint tusers_pk
+			primary key,
+	"Username" varchar not null,
+	"Password" varchar not null
+);
+
+
