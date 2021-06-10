d14c85e7520aea8f156279dbba58ffed82f91d88
savianl <lorenzo.savian@corvallis.it>
2021-06-10 12:15:22 +0200

    init login

---
+++ src/app/auth/token.interceptor.ts
@@ -0,0 +1,16 @@
+// import { Injectable } from '@angular/core';
+// import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+
+// @Injectable()
+// export class TokenInterceptor  implements HttpInterceptor{
+//     constructor(){}
+//     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+//         request = request.clone({
+//             setHeaders:{
+//                 Authorization : "Bearer " + sessionStorage.getItem('jwt')
+//             }
+//         });
+//         return next.handle(request);
+//     }
+// }
