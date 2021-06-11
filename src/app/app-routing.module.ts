8023c2fa2f8e5285f33aa8c447df2a7ca5f415bd
ITSPisaniM <marco.pisani@tecnicosuperiorekennedy.it>
2021-06-11 14:58:28 +0200

    Added and fixed Navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 //components for routing
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
 
 const routes: Routes = [
   {
