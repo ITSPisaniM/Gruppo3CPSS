743206675f700928c774fb91927c4470f8b6b01f
ITSPisaniM <marco.pisani@tecnicosuperiorekennedy.it>
2021-07-08 20:29:42 +0200

    Added acquisti dettaglio

--- src/app/pages/acquisti/acquisti-dettaglio/acquisti-dettaglio.component.ts
+++ src/app/pages/acquisti/acquisti-dettaglio/acquisti-dettaglio.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Acquisto } from 'src/app/models/acquisto';
 
@@ -7,10 +7,15 @@ import { Acquisto } from 'src/app/models/acquisto';
   templateUrl: './acquisti-dettaglio.component.html',
   styleUrls: ['./acquisti-dettaglio.component.scss'],
 })
-export class AcquistiDettaglioComponent implements OnInit {
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public acquisto: Acquisto
-  ) {}
-
-  ngOnInit(): void {}
+export class AcquistiDettaglioComponent {
+  displayedColumns: string[] = [
+    'purchasesItemsId',
+    'purchaseId',
+    'asin',
+    'purchasedAmount',
+    'unitPrice',
+  ];
+  constructor(@Inject(MAT_DIALOG_DATA) public acquisto: Acquisto) {
+    console.log(acquisto);
+  }
 }
