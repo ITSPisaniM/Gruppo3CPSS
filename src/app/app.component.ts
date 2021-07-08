c8e035adbc2df919287b083b1a32bdd13f2b4aa7
ITSPisaniM <marco.pisani@tecnicosuperiorekennedy.it>
2021-07-08 11:25:54 +0200

    Added toolbar's buttons ability to be disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { Location } from '@angular/common';
 import { AuthenticationService } from './auth/authentication.service';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,16 +11,22 @@ import { AuthenticationService } from './auth/authentication.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  actualPage: string;
   title = 'AngularCPSS';
   public loginPage: boolean = false;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private location: Location,
+    private router: Router,
     private authService: AuthenticationService
   ) {}
 
   ngOnInit() {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        this.actualPage = val.url;
+      }
+    });
   }
 
   isHandset$: Observable<boolean> = this.breakpointObserver
